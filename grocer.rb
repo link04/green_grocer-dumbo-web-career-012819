5bae530db5a639818cea626deed9e661ed7d8a81
Maximo Bautista <max.bautista97@gmail.com>
2019-01-17 22:09:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,53 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |item|
+    item.each do |key, value|
+      if new_hash[key]
+        new_hash[key][:count] += 1
+      else
+        new_hash[key] = value
+        new_hash[key][:count] = 1
+      end
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  new_hash = {}
+  if coupons.length > 0
+    coupons.each do |coupon|
+      
+    end
+    new_hash
+  else
+    cart
+  end
+ 
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, details|
+    if details[:clearance] == true
+      discount =  20.to_f * details[:price].to_f / 100.0.round
+      details[:price] -= discount
+    end
+  end
 end
 
 def checkout(cart, coupons)
-  # code here
+  total = 0
+ 
+  items = consolidate_cart(cart)
+  items = apply_coupons(items, coupons)  
+  items = apply_clearance(items)
+  
+  items.each do |item, details|
+    total += (details[:price] * details[:count])
+  end
+  total > 100 ? total - 10.to_f * total.to_f / 100.0 : total
+  
 end
+
