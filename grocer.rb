330752f4c7b40a59f54b6c1d17e7466586b257a1
Maximo Bautista <max.bautista97@gmail.com>
2019-01-17 22:23:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,16 +16,29 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  new_hash = {}
-  if coupons.length > 0
-    coupons.each do |coupon|
-      
+   coupons.each do |coupon|
+    coupon_name = coupon[:item]
+    coupon_item_num = coupon[:num]
+    cart_item = cart[coupon_name]
+
+     next if cart_item.nil? || cart_item[:count] < coupon_item_num
+
+     cart_item[:count] -= coupon_item_num
+
+     coupon_in_cart = cart["#{coupon_name} W/COUPON"]
+
+     if coupon_in_cart
+      coupon_in_cart[:count] += 1
+    else
+      cart["#{coupon_name} W/COUPON"] = { 
+        price: coupon[:cost], 
+        clearance: cart_item[:clearance], 
+        count: 1
+      }
     end
-    new_hash
-  else
-    cart
   end
- 
+
+   cart
 end
 
 def apply_clearance(cart)
